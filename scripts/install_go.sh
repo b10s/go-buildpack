fd0d107aff9266e6b5872da99419df35d205176d
Enshin <b1os@bk.ru>
2019-01-25 01:42:44 +0900

    test

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 GO_VERSION="1.11.4"
 
+env
+set
 if [ $CF_STACK == "cflinuxfs2" ]; then
     GO_SHA256="8faf0b1823cf25416aa5ffdac6eef2f105114abe41b31b3d020102ae7661a5ae"
 elif [ $CF_STACK == "cflinuxfs3" ]; then
